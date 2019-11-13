d712c55982c272bca35d5648756a613b59eda9b5
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-11-13 14:44:21 -0800

    chore(various): installed firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Firebase from './firebase';
+import { FirebaseContext } from './context';
 import { MainComponent } from './components';
 import './App.scss';
 
+console.log(process.env.REACT_APP_TEST);
+
 function App() {
   return (
-    <MainComponent />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <MainComponent />
+    </FirebaseContext.Provider>
   );
 }
 
