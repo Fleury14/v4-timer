b5095559ea21831c207d02efd5e78ecd5a2c74f2
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-11-25 14:33:59 -0800

    chore({boss timer): set up keyboard listener, brought in boss icons

--- src/components/boss-timer/boss-timer.js
+++ src/components/boss-timer/boss-timer.js
@@ -1,28 +1,57 @@
 // @flow
 import React, { Component } from 'react';
+import { Clock } from '..';
 
 type Props = {};
 
 type State = {
     isActive: boolean,
+    startTime: number,
+    currentTime: number,
+    pauseTime: number,
+    finished: boolean,
 };
 
 class BossTimer extends Component<Props, State> {
-    
     state = {
         isActive: false,
+        startTime: 0,
+        currentTime: 0,
+        pauseTime: 0,
+        finished: false,
     }
 
     onPress(e: KeyboardEvent) {
-        console.log(e);
-        if (e.key === ' ' && e.shiftKey && e.ctrlKey) {
+        // console.log(e);
+        if (e.key === ' ') {
+            
+            if (!this.state.isActive) {
+                this.setState({ isActive: true });
+            } else {
+                this.setState({ isActive: false });
+            }
         }
     }
 
+    componentDidMount() {
+        document.addEventListener('keyup', this.onPress.bind(this));
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keyup', this.onPress.bind(this));
+    }
+
     render() {
-        const keypress = document.addEventListener('keyup', this.onPress);
+        const { isActive } = this.state;
         return (
-            <div></div>
+            <div>
+            {isActive ? (
+                <React.Fragment>
+                <p>Boss Timer</p>
+                {/* <Clock /> */}
+                </React.Fragment>
+            ) : null}
+            </div>
         );
     }
 }
