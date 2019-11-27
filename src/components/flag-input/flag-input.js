8e24422cc722a99c3a328d36ff6ecc2be6e6b82f
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-11-27 12:21:20 -0800

    feat(flag input): boss timer instructions

--- src/components/flag-input/flag-input.js
+++ src/components/flag-input/flag-input.js
@@ -9,6 +9,9 @@ const FlagInputComponent = (props) => {
                 <FlagInputForm startTime={(flagObj) => props.onStartTimer(flagObj)}/>
             </div>
             <p className="credits">v0.5.0 - Timer made by Fleury14</p>
+            <div className="flag-input-container">
+                <p className="instructions">Boss Timer: To trigger a separate boss timer, push the space bar to both start and stop the timer. You then have the option to select a boss to assign the time to, or push space again to leave it unassigned. You can click on the corresponding time to select a boss later if you so desire.</p>
+            </div>
         </React.Fragment>
     );
 }
