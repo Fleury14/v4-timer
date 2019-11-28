e67d52395990fab5c01915fc855d671e0e3d221e
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-11-28 15:19:25 -0800

    refac(various): use boss icons in objective selector

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -113,6 +113,7 @@ class TimerComponent extends Component<Props, State> {
     }
 
     applyEdit(id: number, title: string) {
+        console.log('editing', id, title)
         const { flagObj } = this.state;
         if (flagObj && flagObj.objectives) {
             const target:void | TObjective = flagObj.objectives.find(obj => obj.id === id);
