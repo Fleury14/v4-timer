49de1b507a0ed25a806abd42f359f33dc87e7ea9
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-12-05 14:36:20 -0800

    chore(package/tests): install enzyme, mount timer

--- src/__tests__/timer-render.js
+++ src/__tests__/timer-render.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { mount } from 'enzyme';
+import Adapater from 'enzyme-adapter-react-16';
 import { Timer } from '../components';
+
+
+Enzyme.configure({ adapter: new Adapater() });
 
 it('timer renders without crashing', () => {
   const div = document.createElement('div');
@@ -19,4 +24,23 @@ it('timer renders without crashing', () => {
 it('timer renders with a null flag objectives without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Timer flagObj={null} reEntry={() => {}} />, div);
+});
+
+describe("Timer Component", () => {
+    test('mounts as expected', () => {
+        const flagObj = {
+            objectives: [{
+                id: 0,
+                title: "Beat Zeromus",
+                time: 0
+            }]
+        };
+        const wrapper = mount(<Timer flagObj={flagObj} reEntry={() => {}} />);
+        
+        // initial state is as expected
+        expect(wrapper.state('timerActive')).toBe(false);
+        expect(wrapper.state('startTime')).toBe(0);
+        expect(wrapper.state('bossTimes')).toHaveLength(0);
+        expect(wrapper.state('currentTime')).toBe(0);
+    });
 });
