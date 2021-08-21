00f1acc2f3b4e187cf16d01af8f0eb215a832989
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-08-21 10:53:14 -0700

    chore(item data): shops, items

---
+++ src/data/note-data.js
@@ -0,0 +1,34 @@
+export const Shops = [
+    "Agart",
+    "Baron",
+    "Cave Eblan",
+    "Dwarf Castle",
+    "Fabul",
+    "Feymarch",
+    "Hummingway",
+    "Kaipo",
+    "Kokkol",
+    "Mysidia",
+    "Mist Village",
+    "Silvera",
+    "Tomra",
+    "Toroia (Item)",
+    "Toroia (Pub)",
+]
+
+export const Items = [
+    "Life",
+    "Cure2",
+    "Cure3",
+    "Heal",
+    "Ether1",
+    "Ether2",
+    "Tent",
+    "Cabin",
+    "Siren",
+    "Starveil",
+    "Bacchus",
+    "Coffin",
+    "HrGlass",
+    "Cursed"
+]
