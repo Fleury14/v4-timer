07ba25f593245c9cfbc338015f224c8e9f203b7a
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-08-21 12:18:52 -0700

    feat(note taking) add shops, items, typing tggle, reset and new line

--- src/components/note-taker/note-taker.js
+++ src/components/note-taker/note-taker.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Shops, Items } from '../../data/note-data';
 import './note-taker.scss';
 
 const NoteTaker = (props) => {
 
   const [notes, setNotes] = useState('');
+  const [allowTyping, setTyping] = useState(false);
+  const [confirmed, confirmReset] = useState(false);
 
   useEffect(() => {
     loadNotes();
@@ -19,14 +22,68 @@ const NoteTaker = (props) => {
     setNotes(val);
   }
 
+  function toggleTyping() {
+    setTyping(!allowTyping);
+  }
+
+  function addItem(val) {
+    handleChange(notes + " " + val);
+  }
+
+  function handleReset() {
+    if (!confirmed) {
+      confirmReset(true)
+    } else {
+      localStorage.setItem("notes", "");
+      setNotes("");
+      confirmReset(false);
+    }
+  }
+
   return (
     <>
       <textarea
         className="notes"
-        rows="20"
+        rows="10"
         value={notes}
-        onChange={(e) => handleChange(e.target.value)}
+        onChange={(e) => {
+          if (allowTyping) handleChange(e.target.value)
+          }}
       />
+      <div className="note-col">
+        <div className="note-button-col">
+          {Shops.map((shop, index) => {
+          return index % 2 === 0 ? <button key={shop} onClick={() => addItem(shop)}>{shop}</button> : null;
+          })}
+        </div>
+        <div className="note-button-col">
+          {Shops.map((shop, index) => {
+          return index % 2 === 1 ? <button key={shop} onClick={() => addItem(shop)}>{shop}</button> : null;
+          })}
+        </div>
+        <div className="note-button-col">
+          {Items.map((item, index) => {
+          return index % 2 === 0 ? <button key={item} onClick={() => addItem(item)}>{item}</button> : null;
+          })}
+        </div>
+        <div className="note-button-col">
+          {Items.map((item, index) => {
+          return index % 2 === 1 ? <button key={item} onClick={() => addItem(item)}>{item}</button> : null;
+          })}
+        </div>
+      </div>
+      <div>
+        <div className="type-row">
+          <input type="checkbox" checked={allowTyping} onClick={() => toggleTyping()}/>
+          <p>Enable Typing</p>
+        </div>
+        <p className="warning">Warning: If you have boss timer/start stop keys bound, typing may trigger them. Please double check your key binds before enabling</p>
+        <div className="bottom-button-row">
+          <button onClick={() => handleReset()}>{!confirmed ? "Reset" : "Are you sure?"}</button>
+          <button onClick={() => addItem(String.fromCharCode(13, 10))}>New Line</button>
+        </div>
+        
+      </div>
     </>
   );
 }
