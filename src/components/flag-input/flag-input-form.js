7086be23c9ed0b728fffd58f16563869b70f3378
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-10-15 14:32:57 -0700

    chore(input form): ladder seaason 4

--- src/components/flag-input/flag-input-form.js
+++ src/components/flag-input/flag-input-form.js
@@ -39,11 +39,11 @@ class FlagInputForm extends Component {
                 <h2 className="flag-preset-title">Ladder Flagsets</h2>
                 <div className="flag-presets">
                     <button onClick={() => this.setState({ flags: 'O1:quest_forge/2:quest_tradepink/3:quest_pass/win:crystal Kmain/trap Pkey Crelaxed/start:fusoya/j:abilities Twildish Sstandard/no:j Bstandard/alt:gauntlet Nchars/key/bosses Etoggle/noexp Gwarp/life/sylph -kit:freedom -supersmith -vanilla:fusoya' })}>Fuwario</button>
-                    <button onClick={() => this.setState({ flags: 'Orandom:3/req:all/win:crystal Kmain Pshop Crelaxed/j:abilities Twild Swild/free/no:apples Bstandard/whyburn Nnone Etoggle Glife/sylph -kit:loaded -kit2:random -spoon' })}>Supermarket Sweep</button>
-                    <button onClick={() => this.setState({ flags: 'O1:quest_forge/2:quest_tradepink/3:quest_magnes/random:2,gated_quest/req:4/win:crystal Kmain/summon/moon Pkey Crelaxed/distinct:10/j:abilities/nekkie/bye Twildish/maxtier:6 Sstandard Bstandard/alt:gauntlet Nchars/key Etoggle Glife/sylph -kit:basic -kit2:defense -noadamants -spoon -exp:noboost -vanilla:giant' })}>Lali-ho Redux</button>                    
+                    <button onClick={() => this.setState({ flags: 'Orandom:4,gated_quest,boss/req:3/win:crystal Kmain Pshop Crelaxed/j:abilities Twild Swild/free/no:apples Bstandard/whyburn Nnone Etoggle Glife/sylph -spoon' })}>Supermarket Sweep</button>
+                    
                 </div>
                 <div className="flag-presets">
-                    <button onClick={() => this.setState({ flags: 'Orandom:5,gated_quest/req:4/win:crystal Kmain/summon/moon Pkey Cstandard/no:fusoya/j:abilities/bye/permajoin Tpro Scabins/free Bstandard/alt:gauntlet Nchars/key Etoggle Gwarp/life/sylph -kit:basic -noadamants' })}>Finders Keepers</button>
+                    <button onClick={() => this.setState({ flags: 'Orandom:5,gated_quest/win:crystal Kmain/summon/moon Pkey Cstandard/distinct:7/j:abilities/nodupes/bye Tpro Spro/sell:quarter Bstandard/alt:gauntlet Nchars/key Etoggle Glife/sylph -kit:basic -noadamants' })}>FG Mk.II</button>                    
                     <button onClick={() => this.setState({ flags: 'Omode:classicgiant/win:game Kmain/moon Pnone Cstandard Twild/no:j Scabins/free Bstandard/whichburn Nnone Etoggle/no:jdrops Gdupe/mp/warp/life/sylph -kit:basic -noadamants -spoon -supersmith' })}>Giant %</button>
                 </div>
                 <h2 className="flag-preset-title">Tournament Flagsets</h2>
