47c9767b117a59cd3724ccd9e66a05fdc7f38c3e
J.R <fleury14@comcast.net>
2021-12-21 16:52:12 -0800

    chore(data): add trapped chests

---
+++ src/data/trapped-chests.js
@@ -0,0 +1,170 @@
+const trapped = [
+    {
+        title: "Tower of Zot",
+        chests: [
+            {
+                location: "2F, only chest",
+                enemy: "FlameDog x 1"
+            }
+        ]
+    },
+    {
+        title: "Eblan Cave",
+        chests: [
+            {
+                location: "Behind the save point",
+                enemy: "Staleman x 2"
+            }
+        ]
+    },
+    {
+        title: "Tower of Bab-il (Upper)",
+        chests: [
+            {
+                location: "2F, across the bridge",
+                enemy: "MadOgre * 4"
+            }
+        ]
+    },
+    {
+        title: "Castle Eblan",
+        chests: [
+            {
+                location: "Left Tower, 1F behind hidden path",
+                enemy: "Stalemen x 1&#13;Skull x 4"
+            },
+            {
+                location: "Right Tower, behind dropdown",
+                enemy: "Lamia x 1&#13;Black Cat x 2"
+            },
+            {
+                location: "Basement, right chest",
+                enemy: "Mad Ogre x 3"
+            }
+        ]
+    },
+    {
+        title: "Giant of Babil",
+        chests: [
+            {
+                location: "4f, last giant chest",
+                enemy: "Last Arm x 1"
+            }
+        ]
+    },
+    {
+        title: "Feymarch",
+        chests: [
+            {
+                location: "B3F, behind the entrance to town",
+                enemy: "Warrior x 5"
+            }
+        ]
+    },
+    {
+        title: "Tower of Bab-il (Lower)",
+        chests: [
+            {
+                location: "2F, bottom room",
+                enemy: "Alert x 1&#13;(Summons Stoneman)"
+            },
+            {
+                location: "2F, right room",
+                enemy: "Alert x 1&#13;(Summons Chimera)"
+            },
+            {
+                location: "4F, right side, bottom left room",
+                enemy: "Alert x 1&#13;(Summons Naga)"
+            },
+            {
+                location: "4F, right side, top room",
+                enemy: "Alert x 1&#13;(Summons FlameDog)"
+            }
+        ]
+    },
+    {
+        title: "Sylph Cave",
+        chests: [
+            {
+                location: "B2F, ring around the rosy chest on the Yang path",
+                enemy: "ToadLady x 1&#13;Tiny Toad x 6"
+            },
+            {
+                location: "Avenger room, top left",
+                enemy: "Ghost x 6"
+            },
+            {
+                location: "Avenger room, top middle",
+                enemy: "Ghost x 6"
+            },
+            {
+                location: "Avenger room, top right",
+                enemy: "Ghost x 6"
+            },
+            {
+                location: "Avenger room, bottom left",
+                enemy: "Molbol x 2&#13;DarkTree x 2"
+            },
+            {
+                location: "Avenger room, bottom middle",
+                enemy: "Molbol x 2"
+            },
+            {
+                location: "Avenger room, bottom right",
+                enemy: "Centpede x 2"
+            }  
+        ]
+    },
+    {
+        title: "Lunar Path",
+        chests: [
+            {
+                location: "Bottom chest",
+                enemy: "Procyote x 2&#13;Juclyote x 2"
+            }
+        ]
+    },
+    {
+        title: "Lunar Subterrain",
+        chests: [
+            {
+                location: "B1F, Bottom right (loneliest chest)",
+                enemy: "RedGiant x 2"
+            },
+            {
+                location: "B2F en route to Murasame altar",
+                enemy: "Warlock x 2&#13;Kary x 2"
+            },
+            {
+                location: "B3F, bottom tier left side",
+                enemy: "Behemoth x 1"
+            },
+            {
+                location: "B4F, on Crystal sword altar bridge",
+                enemy: "Behemoth x 1"
+            },
+            {
+                location: "B4F, 2nd tier from top (going up at the fork)",
+                enemy: "Behemoth x 1"
+            },
+            {
+                location: "B4F, first chest upon entering",
+                enemy: "Red D x 1&#13;Blue D x 1"
+            },
+            {
+                location: "B4F, 3rd tier from top (going down at the fork)",
+                enemy: "Red D x 2"
+            },
+            {
+                location: "B4F, bottom tier, far left",
+                enemy: "Warlock x 1&#13;D. Fossil x 1"
+            },
+            {
+                location: "B4F, V shaped room",
+                enemy: "Warlock x 1&#13;Kary x 3"
+            }
+        ]
+    },
+];
+
+export default trapped;
